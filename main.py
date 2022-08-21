7c41bff596dfc382c187cf67fc3dbebd0a38c615
daniel.waprzko <daniel.waprzko@outlook.com>
2022-08-21 22:23:15 +0200

    Merged function with line creating with load_image

--- main.py
+++ main.py
@@ -3,22 +3,60 @@ import cv2
 import numpy as np
 
 
-# Python function to read image using OpenCV and to find a red dot coordinates that should be on the middle of the
+# Python function to read image, and set up the lines
 def load_image():
 	img = cv2.imread("Acacia_1.jpeg")
-	red_pixels = np.argwhere(cv2.inRange(img, (0, 0, 250), (0, 0, 255)))
-	for px, py in red_pixels:
+	Red_pixels = np.argwhere(cv2.inRange(img, (0, 0, 251), (0, 0, 255)))
+	for px, py in Red_pixels:
 		cv2.circle(img, (py, px), 5, (0, 255, 255), 1)
 	cv2.imwrite("out.jpeg", img)
 
-# Python function to set multiple line from red dot to border of image
-def set_line():
-	print("set_line!")
+	# Calculating the size of image
+	height, width = img.shape[:2]
+	halfwidth = int(width/2)
+	halfheight = int(height/2)
+
+	# Set the points for lines
+	center = (py, px)
+	line1 = (0,0)
+	line2 = (0, width)
+	line3 = (height, width)
+	line4 = (height, 0)
+	line5 = (height, halfwidth)
+	line6 = (halfheight, width)
+	line7 = (halfheight, 0)
+	line8 = (0, halfwidth)
+
+	# Line Color in BGR Format
+	color = (1, 0, 0) # will be Blue
+
+	# Thickness and line type
+	thickness = 2
+	linetype = cv2.LINE_AA
+
+	# Draw lines on image
+	img = cv2.line(img, center, line1, color, thickness, linetype)
+	img = cv2.line(img, center, line2, color, thickness, linetype)
+	img = cv2.line(img, center, line3, color, thickness, linetype)
+	img = cv2.line(img, center, line4, color, thickness, linetype)
+	img = cv2.line(img, center, line5, color, thickness, linetype)
+	img = cv2.line(img, center, line6, color, thickness, linetype)
+	img = cv2.line(img, center, line7, color, thickness, linetype)
+	img = cv2.line(img, center, line8, color, thickness, linetype)
+
+	# show image
+	cv2.imshow("out.jpeg", img)
+	cv2.waitKey(0)
+
+
 
 # Python function to count changes of colour and brightness on multiple lines, 
 # to separate winter and summer time and calculate age of tree for every line and store it in a table
 def count_color_changes():
 	print("count_color_changes!")
+
+
+
 
 
 # Python function to compare values of multiple lines and make a decision acording to rules
@@ -32,9 +70,7 @@ def main():
     print("main!")
 
 
-
 load_image()
-set_line()
 count_color_changes()
 compare_lines()
 main()
