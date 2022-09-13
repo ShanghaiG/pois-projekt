b9d2608c5930b5ac7b0ef0cdae25709610bb7228
Kanciachu <78599864+Kanciachu@users.noreply.github.com>
2022-09-13 18:42:13 +0200

    v3
    added crack(s) detection (methheads warning)

--- DATA_ANALISYS.py
+++ DATA_ANALISYS.py
@@ -1,11 +1,14 @@
+import numpy
+
 from calculate_methods import *
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 from skimage.io import imread_collection
-
+import copy
 
 img = cv2.imread("Images/jeszcze_ladniejsza_nazwa.png")
+img_to_analisys = copy.deepcopy(img)
 center = calculate_center(img)
 height, width = image_size(img)
 img = create_threshold(img)
@@ -44,6 +47,7 @@ def produce_plots(data, name, dir):
 
             dane = []
 
+
 def compare_plots(dir):
     col_dir = f'{dir}/*.png'
     images = imread_collection(col_dir)
@@ -52,10 +56,31 @@ def compare_plots(dir):
 
     plt.imshow(images[1])
 
-    # for i in range(1,16):
-    #     fig.add_subplot(8, 1, i)
-    #     plt.imshow(images[i])
-    #     plt.show()
 
+def crack_analisys(image):
+
+    # Calculate amount of pixels on screen that are not white (background)
+    image_without_white_color = np.argwhere(cv2.inRange(image, (0, 0, 0), (250, 250, 250)))
+    pixels_amount = len(image_without_white_color)
+
+    # Find dark pixels associated with cracks
+    dark_pixels = np.argwhere(cv2.inRange(image, (0, 0, 0), (3, 3, 3)))
+
+    # create canvas with 0's for visualization in shape of orginal image
+    canvas = numpy.zeros([image.shape[0], image.shape[1]])
+
+    # Replace places where dark pixels are with 1's
+    for pixel in dark_pixels:
+        canvas[pixel[0]][pixel[1]] = 1
+
+    # Display graph
+    plt.imshow(canvas, cmap='hot', interpolation='nearest')
+    plt.show()
+
+    # Print message with calculated amount
+    print(f"Tree have {round((len(dark_pixels) / pixels_amount),2)} % of cracks")
+
+
+crack_analisys(img_to_analisys)
 produce_plots([summary_lines_points,reduced_summary_points],['reduced_summary', 'summary_lines'],'data_images')
 compare_plots("data_images")
